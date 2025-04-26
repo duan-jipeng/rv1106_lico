8e7de6a71ea8b43acda8570f2050b2ed933bf18a
peng <jipeng.duan@outlook.com>
2025-04-26 12:49:17 +0800

    touch rotate achieved

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # rv1106_demo
 
 1.st7789 show error, change the HSD20_IPS value to 0 in fb_st7789v.c file;
-2.ft6336u needs to be configed in kernelconfig(focaltect touchscreen, by seaching goodix in kernelmenuconfig);
+2.ft6336u needs to be configed in kernelconfig(focaltect touchscreen, by seaching goodix in kernelmenuconfig), and remove focaltech_test folder;
+3.ft6336u touch rotate,  config input_report_abs fo focaltech_core.c(https://blog.csdn.net/Shushan1/article/details/88661732)
--- build_lico.sh
+++ build_lico.sh
@@ -1,3 +1,4 @@
+# 1.system
 cp -rf ./lico_project_file/build.sh  ./sdk/project
 cp -rf ./lico_project_file/BoardConfig-SPI_NAND-Buildroot-RV1106_Lico.mk ./sdk/project/cfg/BoardConfig_IPC
 cp -rf ./lico_project_file/rv1106g-lico.dts ./sdk/sysdrv/source/kernel/arch/arm/boot/dts
@@ -6,16 +7,21 @@ cp -rf ./lico_project_file/lico_rv1106_uboot_defconfig ./sdk/sysdrv/source/uboot
 cp -rf ./lico_project_file/lico_rv1106_linux_defconfig ./sdk/sysdrv/source/kernel/arch/arm/configs
 cp -rf ./lico_project_file/lico_defconfig ./sdk/sysdrv/tools/board/buildroot
 cp -rf ./lico_project_file/make_fit_uboot.sh ./sdk/sysdrv/source/uboot/u-boot/arch/arm/mach-rockchip
-cp -rf ./lico_project_file/Makefile ./sdk/sysdrv
+cp -rf ./lico_project_file/Makefile_sysdrv ./sdk/sysdrv/Makefile
 
 
+# 2.screen and touch
 cp -rf ./lico_project_file/fb_st7789v.c ./sdk/sysdrv/source/kernel/drivers/staging/fbtft
 
-# cp -rf ./lico_project_file/focaltech_core.c ./sdk/sysdrv/source/kernel/drivers/input/touchscreen/focaltech_touch
-# cp -rf ./lico_project_file/focaltech_core.h ./sdk/sysdrv/source/kernel/drivers/input/touchscreen/focaltech_touch
 # ./build.sh kernelconfig focaltect y
+rm -rf ./sdk/sysdrv/source/kernel/drivers/input/touchscreen/focaltech_touch/focaltech_test
+cp -rf ./lico_project_file/Makefile_focaltech ./sdk/sysdrv/source/kernel/drivers/input/touchscreen/focaltech_touch/Makefile
+
+cp -rf ./lico_project_file/focaltech_core.c ./sdk/sysdrv/source/kernel/drivers/input/touchscreen/focaltech_touch
+cp -rf ./lico_project_file/focaltech_core.h ./sdk/sysdrv/source/kernel/drivers/input/touchscreen/focaltech_touch
 
 
+# 3.wifi
 mkdir -p ./sdk/external/lib/firmware/rtlwifi/
 
 
